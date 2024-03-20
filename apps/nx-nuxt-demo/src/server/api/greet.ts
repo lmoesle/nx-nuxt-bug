f5f03799d54d11fcb0e73f8491def799ea67d769
Lukas Mösle <lukas95m@yahoo.de>
2024-03-20 08:46:42 +0100

    reproduce nx nuxt import bug

--- apps/nx-nuxt-demo/src/server/api/greet.ts
+++ apps/nx-nuxt-demo/src/server/api/greet.ts
@@ -1,8 +1,11 @@
 import { defineEventHandler, getQuery } from 'h3';
+import { demo } from '@nx-nuxt-demo/demo';
 
 export default defineEventHandler((event) => {
   const q = getQuery(event);
   const name = q.name || 'World';
+
+  console.log(demo());
 
   return {
     message: `Hello ${name}`,
---
+++ demo/README.md
@@ -0,0 +1,7 @@
+# demo
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build demo` to build the library.
